ffc9184e80bfbe9f71057b82ff342ff5fc0b8160
Arthut Kelvim <kelvimarthur@gmail.com>
2022-02-02 12:11:18 -0300

    Foi iniciado o Header

--- src/components/Home/Filtros.js
+++ src/components/Home/Filtros.js
@@ -1,9 +1,48 @@
 import React from "react";
 
+
 class Filtros extends React.Component{
+
+    state = {
+        valorMinimo: '',
+        valorMaximo: '',
+        buscar: ''
+    }
+
+    onChangeMinimo = (event) =>{
+        this.setState({
+            valorMinimo: event.target.value
+        })
+    }
+    onChangeMaximo = (event) =>{
+        this.setState({
+            valorMaximo: event.target.value
+        })
+    }
+    onChangebuscar = (event) =>{
+        this.setState({
+            buscar: event.target.value
+        })
+    }
+
     render(){
+        
+        console.log("VALOR MINIMO:",this.state.valorMinimo, "VALOR MAXIMO", this.state.valorMaximo, "BUSCAR:", this.state.buscar)
+        
+        
+
         return(
             <>
+                <div>
+                    <input type="number" placeholder="Valor mínimo" onChange={this.onChangeMinimo} value={this.state.valorMinimo}/>
+                    <input type="number" placeholder="Valor máximo" onChange={this.onChangeMaximo} value={this.state.valorMaximo}/>
+                    <input type="text" placeholder="Buscar" onChange={this.onChangebuscar} value={this.state.buscar}/>
+                    <select name="" id="">
+                        <option value="">Titulo</option>
+                        <option value="">Valor</option>
+                        <option value="">Prazo</option>
+                    </select>
+                </div>
                 
             </>
         )
---
+++ src/components/Home/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Header extends React.Component{
+    render(){
+        return(
+            <>
+                <div>
+                    <h1>Ninjas ♥</h1>
+                </div>
+                
+            </>
+        )
+    }
+}
+
+export default Header
