d10d5680a68a8e556aff11f869fafcca3e958dc0
Arthut Kelvim <kelvimarthur@gmail.com>
2022-02-02 14:22:36 -0300

    Foi adicionado o MVP do Header e do Footer por completo

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,14 +1,53 @@
 import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
+import styled from "styled-components";
+
+const Fig = {
+    fontSize: "40px",
+    color: "#000",
+    paddingRight: "50px",
+    cursor: "pointer"
+}
+const Content = styled.div`
+    background-color: brown;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+const H1 = styled.h1`
+    padding: 0px 100px;
+`
+const Button = styled.button`
+    height: 30px;
+    width: 150px;
+    cursor: pointer;
+    font-size: 18px;
+    font-weight: bold;
+    border: 1px solid #fff;
+    background-color: brown;
+    border-radius: 5px;
+    &:hover{
+        background-color: #c74040;
+    } 
+`
 
 class Header extends React.Component{
     render(){
         return(
-            <>
+            <Content>
                 <div>
-                    <h1>Ninjas ♥</h1>
+                    <H1>Ninjas ♥</H1>
                 </div>
-                
-            </>
+                <div>
+                    <Button>Seja um ninja!</Button>
+                </div>
+                <div>
+                    <Button>Entrar</Button>
+                </div>
+                <div>
+                    <FaShoppingCart style={Fig}/>
+                </div>
+            </Content>
         )
     }
 }
