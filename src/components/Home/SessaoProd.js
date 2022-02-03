04c5a320b2ff97fc73d4ada6a2ffb03034494784
Leo Rigotto <leorigotto@me.com>
2022-02-03 16:01:51 -0300

    filtro e botao remover do carrinho ajustados

--- src/components/Home/SessaoProd.js
+++ src/components/Home/SessaoProd.js
@@ -20,9 +20,10 @@ export default class SessaoProd extends React.Component {
     state = {
       jobs: [],
       cart: [],
-      valorMinimo: '',
-      valorMaximo: '',
-      buscar: ''
+      valorMinimo: "",
+      valorMaximo: "",
+      buscar: '',
+      order: "title",
     }
 
     getJobs = () => {
@@ -45,6 +46,16 @@ export default class SessaoProd extends React.Component {
       this.setState({cart: newCart}) 
     }
 
+    removeCarrinho = (ev) => {
+      const idJob = ev.target.value
+      const novoCarrinho = this.state.cart.filter((job) => {
+          return job.id !== idJob
+      }).map((job) => {
+          return job
+      })
+      this.setState({cart: novoCarrinho})
+  }
+
     //A partir daqui: Arthur
     onChangeMinimo = (event) =>{
       this.setState({
@@ -61,13 +72,38 @@ export default class SessaoProd extends React.Component {
           buscar: event.target.value
       })
     }
+    onChangeOrder = (e) => {
+      this.setState({
+        order: e.target.value
+    })}
     
     render() {
       
       let allJobs
 
       if (this.state.jobs !== []){
-        allJobs = this.state.jobs.map((job) => {
+        allJobs = this.state.jobs
+        .filter((job) => {
+          return job.title.toLowerCase().includes(this.state.buscar.toLowerCase())
+          })
+          .filter((job) => {
+          return this.state.valorMinimo === "" || job.price >= Number(this.state.valorMinimo)
+        })
+        .filter((job) => {
+          return this.state.valorMaximo === "" || job.price <= Number(this.state.valorMaximo)
+        })
+        .sort((a, b) => {
+            switch (this.state.order) {
+              case "price":
+                return a.price - b.price
+
+              case "title":
+                return a.title.localeCompare(b.title)
+
+              case "dueDate":
+                return new Date(a.dueDate).getTime() < new Date(b.dueDate).getTime()
+            }})
+        .map((job) => {
           return <CardProd 
           key={job.id}
           cardTitle={job.title} 
@@ -78,23 +114,27 @@ export default class SessaoProd extends React.Component {
           addToCart={this.addToCart}
           value={job.id}/>
         })
-        
       }
-      console.log("sou jobs",this.state.jobs)
+
       return (
         <SessionContainer>
+
                 <div>
                     <input type="number" placeholder="Valor mínimo" onChange={this.onChangeMinimo} value={this.state.valorMinimo}/>
                     <input type="number" placeholder="Valor máximo" onChange={this.onChangeMaximo} value={this.state.valorMaximo}/>
                     <input type="text" placeholder="Buscar" onChange={this.onChangebuscar} value={this.state.buscar}/>
-                    <select name="" id="">
-                        <option value="">Titulo</option>
-                        <option value="">Valor</option>
-                        <option value="">Prazo</option>
+                    <select name="ordem" onChange={this.onChangeOrder}>
+                        <option value="title">Titulo</option>
+                        <option value="price">Valor</option>
+                        <option value="dueDate">Prazo</option>
                     </select>
                 </div>
-            <Carrinho cart={this.state.cart}/>
+
+
+            <Carrinho cart={this.state.cart} remove={this.removeCarrinho}/>
+            
             {allJobs}
+
         </SessionContainer>
       )
     }
